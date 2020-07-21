d6b87d73726c8ae2b85c7f5e03bc58404a2da670
Leandro Leite <lleite.ds@gmail.com>
2020-07-20 22:48:07 -0300

    Add files via upload

---
+++ Entities/TaxPayerCompany.cs
@@ -0,0 +1,28 @@
+﻿namespace Aula_HerancaPolimorfismo_Encapsulamento.Entities
+{
+    class TaxPayerCompany : TaxPayer
+    {
+        public int NumberEmployees { get; set; }
+
+        public TaxPayerCompany(string name, double annualIncome, int numberEmployees) : base(name, annualIncome)
+        {
+            NumberEmployees = numberEmployees;
+        }
+
+        public override double TaxCalculate()
+        {
+            double tax = 0.0;
+
+            if (NumberEmployees <= 10)
+            {
+                tax = AnnualIncome * 0.16;
+            }
+            else
+            {
+                tax = AnnualIncome * 0.14;
+            }
+
+            return tax;
+        }
+    }
+}
---
+++ Entities/TaxPayerIndividual.cs
@@ -0,0 +1,33 @@
+﻿namespace Aula_HerancaPolimorfismo_Encapsulamento.Entities
+{
+    class TaxPayerIndividual : TaxPayer
+    {
+        public double HealthExpeditures { get; set; }
+
+        public TaxPayerIndividual(string name, double annualIncome, double healthExpeditures) : base(name, annualIncome)
+        {
+            HealthExpeditures = healthExpeditures;
+        }
+
+        public override double TaxCalculate()
+        {
+            double tax = 0.0;
+
+            if (AnnualIncome < 20000.00)
+            {
+                tax = AnnualIncome * 0.15;
+            }
+            else if (AnnualIncome >= 20000.00)
+            {
+                tax = AnnualIncome * 0.25;
+            }
+
+            if (HealthExpeditures > 0)
+            {
+                tax -= HealthExpeditures * 0.50;
+            }
+
+            return tax;
+        }
+    }
+}
